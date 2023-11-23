1bb4e0566bcfff73694579971a1820df3b1aa50d
Kapil <kapilbehwal9555@gmail.com>
2023-11-23 18:11:31 +0530

    first c

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import {BrowserRouter as Router , Route,Routes} from 'react-router-dom'
+import ControlData from './components/ControlData';
+import FormScreen from './components/FormScreen';
+
 
 function App() {
+  const [data , setData] = useState({
+    input1:"",
+    input2:"",
+    input3:"",
+    input4:"",
+    input5:"",
+    input6:"",
+    input7:"",
+    input8:"",
+    input9:"",
+    input10:"",
+    input11:"",
+    input12:"",
+    input13:"",
+    input14:"",
+    input15:"",
+    input16:"",
+    input17:"",
+    input18:"",
+    input19:"",
+    input20:"",
+    input21:"",
+    input22:"",
+  
+
+})
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+     <Router>
+        <Routes>
+          <Route path='/' element={<ControlData data={data}  setData={setData}/>}/>
+          <Route path='/form' element={<FormScreen data={data}  setData={setData}/>}/>
+        </Routes>
+     </Router>
+    </>
   );
 }
 
